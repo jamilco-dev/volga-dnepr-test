a58091c7e0a7bb4029ae8652ed615ed67fd0dfba
Andrey Yudin <yudin_ag@ursip.ru>
2023-08-22 12:29:03 +0300

    .

--- Python/Task1/main.py
+++ Python/Task1/main.py
@@ -1,4 +1,14 @@
 import pandas as pd
 
+import pickle
+
+
+def read():
+    file = open("Expl/ILVD23.CYC", "rb")
+    data = pickle.load(file)
+    file.close()
+    print(data)
+
+
 if __name__ == "__main__":
-    pd.test()
+    read()
