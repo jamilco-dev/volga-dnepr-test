0c30caa1cb0af3aee976e33ae55eaa6d548dc601
Andrey Yudin <yudin_ag@ursip.ru>
2023-08-23 00:41:52 +0300

    .

--- Python/Task1/main.py
+++ Python/Task1/main.py
@@ -2,9 +2,21 @@ import numpy as np
 import pandas as pd
 
 
+def reverse_bits(n, no_of_bits=16):
+    result = 0
+    for i in range(no_of_bits):
+        result <<= 1
+        result |= n & 1
+        n >>= 1
+    return result
+
+
 def readCYCNP():
     with open("Expl/76511121022p-1.dat", "rb") as file:
         data = np.fromfile(file, dtype=np.int16)
+        # print(data)
+        # data = np.array([reverse_bits(val) for val in data])
+        # print(data)
 
         indices_or_sections = len(data) // 256
         data = np.array_split(data, indices_or_sections)
@@ -12,5 +24,14 @@ def readCYCNP():
         print(data)
 
 
+def a():
+    arr = np.arange(11)
+
+    arr = np.array([reverse_bits(val) for val in arr])
+
+    print(arr)
+
+
 if __name__ == "__main__":
     readCYCNP()
+    # a()
