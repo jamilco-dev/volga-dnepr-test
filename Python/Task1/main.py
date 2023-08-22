58eef3e2f037e5ad70ba43aaec6d9c21b2da888b
Andrey Yudin <yudin_ag@ursip.ru>
2023-08-22 03:26:04 +0300

    .

--- Python/Task1/main.py
+++ Python/Task1/main.py
@@ -32,16 +32,8 @@ def rpkm(counts, lengths):
 
 
 if __name__ == "__main__":
-    # array1d = np.array([1, 2, 3, 4])
-    # print(array1d)
-    # print(type(array1d))
-    # print(array1d.shape)
-    gene0 = [100, 200]
-    gene1 = [50, 0]
-    gene2 = [350, 100]
-    expression_data = [gene0, gene1, gene2]
+    x = np.array([1, 2, 3, 4])
+    print(x * 2)
 
-    array2d = np.array(expression_data)
-    print(array2d)
-    print(array2d.shape)
-    print(type(array2d))
+    y = np.array([0, 1, 2, 1])
+    print(x + y)
