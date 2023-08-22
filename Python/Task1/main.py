138edd415fb74100637f80bf25d15a6543dedfab
Andrey Yudin <yudin_ag@ursip.ru>
2023-08-22 20:24:56 +0300

    .

--- Python/Task1/main.py
+++ Python/Task1/main.py
@@ -7,10 +7,8 @@ def readCYCNP():
         data = np.fromfile(file, dtype=np.int16)
 
         indices_or_sections = len(data) // 256
-        print(indices_or_sections)
         data = np.array_split(data, indices_or_sections)
         data = pd.DataFrame(data)
-        print(len(data))
         print(data)
 
 
