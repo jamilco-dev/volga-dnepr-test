ac946d6c2cb90c9ed15c8c15d584f02cacc568de
Andrey Yudin <yudin_ag@ursip.ru>
2023-08-22 13:24:18 +0300

    .

--- Python/Task1/main.py
+++ Python/Task1/main.py
@@ -1,14 +1,30 @@
-import pandas as pd
-
 import pickle
 
 
+def readdCYC():
+    file=open("Expl/ILVD23.CYC","rb")
+    data=bytearray(file.read(2))
+    i = 1
+    while len(data) > 0:
+        print (f"{i}: {data}")
+        data=bytearray(file.read(2))
+        i +=1
+    file.close()
+
+def write():
+    file = open("binary.dat",'wb')
+    x = [1,2,3,4,5]    #data we wrote in file
+    pickle.dump(x,file)
+    file.close()
+
 def read():
-    file = open("Expl/ILVD23.CYC", "rb")
+    file = open("binary.dat",'rb')
     data = pickle.load(file)
     file.close()
     print(data)
 
 
 if __name__ == "__main__":
-    read()
+    # write()
+    # read()
+    readdCYC()
