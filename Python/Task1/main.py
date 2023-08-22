2c0a49b579357c6dd6eec42fd313f599384fc9cb
Andrey Yudin <yudin_ag@ursip.ru>
2023-08-23 01:21:41 +0300

    .

--- Python/Task1/main.py
+++ Python/Task1/main.py
@@ -13,15 +13,39 @@ def reverse_bits(n, no_of_bits=16):
 
 def readCYCNP():
     with open("Expl/76511121022p-1.dat", "rb") as file:
+        # Извлекаем данные в массив с элементами по два байта
         data = np.fromfile(file, dtype=np.int16)
-        # print(data)
-        # data = np.array([reverse_bits(val) for val in data])
-        # print(data)
 
+        # Вычисляем количество кадров
         indices_or_sections = len(data) // 256
+
+        # Получаем 2-х мерный массив кадров по 256 слов, слово = 2байта
         data = np.array_split(data, indices_or_sections)
+
+        # Получаем матрицу кадр-канал
         data = pd.DataFrame(data)
+
+        # Выбираем 6-й канал
+        data = data[5]
+
+        # Фильтруем не нулевые
+        data = data[data != 0]
+
+        # Применяем маску выбора
+        data = data.apply(lambda x: x & 4095)
+
+        # Реверсируем биты
+        data = data.apply(reverse_bits)
+
+        # Умножаем на коэфф
+        data = data.apply(lambda x: x * 0.3048)
+
+        # Фильтруем по значению > 10
+        data = data[data > 10]
+
         print(data)
+
+        print(len(data))
 
 
 def a():
