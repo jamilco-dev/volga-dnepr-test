4cf0088b505e198948b506f4481f03545596ee3d
Andrey Yudin <yudin_ag@ursip.ru>
2023-08-22 20:14:53 +0300

    .

--- Python/Task1/main.py
+++ Python/Task1/main.py
@@ -3,21 +3,16 @@ import pandas as pd
 
 
 def readCYCNP():
-    with open("Expl/76511121022p-1.dat","rb") as file:
-        data = np.fromfile(file,dtype=np.int16)
+    with open("Expl/76511121022p-1.dat", "rb") as file:
+        data = np.fromfile(file, dtype=np.int16)
 
-        indices_or_sections = len(data)//256
+        indices_or_sections = len(data) // 256
         print(indices_or_sections)
         data = np.array_split(data, indices_or_sections)
         data = pd.DataFrame(data)
         print(len(data))
         print(data)
 
+
 if __name__ == "__main__":
-    # write()
-    # read()
-    # readCYC()
     readCYCNP()
-    # x = np.arange(8.0)
-    # x = np.array_split(x, 3)
-    # print(x)
